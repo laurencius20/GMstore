8d279c16f6c6dd54055179932d512362c0e24f80
laurencius20 <louissaga20@gmail.com>
2023-05-22 18:16:31 +0700

    config for deployment in render.com

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import connectToDatabase from './database.js';
 import express from 'express';
 
@@ -7,8 +8,8 @@ import productRoutes from './routes/productRoutes.js';
 import userRouters from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
+import path from 'path';
 
-dotenv.config();
 connectToDatabase();
 const app = express();
 
@@ -20,6 +21,14 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
 
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(port, () => {
   console.log(`Server runs on port ${port}.`);
