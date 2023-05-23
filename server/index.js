98eed8a75d6bbe702b1de159ecbbbc32d6969d9a
laurencius20 <louissaga20@gmail.com>
2023-05-23 15:55:05 +0700

    adjust size upload for payment proof

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ import path from 'path';
 connectToDatabase();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRouters);
