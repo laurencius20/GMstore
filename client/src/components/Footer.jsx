3768f3d3211127b67d6ce5197dc263aec568da1e
laurencius20 <louissaga20@gmail.com>
2023-05-27 18:55:17 +0700

    new revision

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,9 +9,11 @@ import {
   useColorModeValue as mode,
   Flex,
   Icon,
+  // Button,
 } from '@chakra-ui/react';
+import { AiFillShop, AiOutlineMail } from 'react-icons/ai';
+import { BiPhoneCall } from 'react-icons/bi';
 import { FaWhatsapp } from 'react-icons/fa';
-import { AiFillShop, AiOutlineMail, AiOutlineInstagram } from 'react-icons/ai';
 
 const Footer = () => (
   <Box w='100%' bg={mode('gray.100', 'gray.900')}>
@@ -20,9 +22,9 @@ const Footer = () => (
         spacing='8'
         direction={{ base: 'column', md: 'row' }}
         justify='space-between'
-        py={{ base: '12', md: '16' }}
+        py={{ base: '12', md: '16', lg: '10' }}
       >
-        <Stack spacing={{ base: '6', md: '8' }} align='start'>
+        <Stack spacing={{ base: '6', md: '8', lg: '4' }} align='start'>
           <Flex alignItems='center'>
             <Icon as={AiFillShop} h={10} w={10} color='blue.500' />
             <Text fontSize='2xl' fontWeight='extrabold' pl='2'>
@@ -31,6 +33,13 @@ const Footer = () => (
           </Flex>
           <Text color='muted' fontWeight='semibold'>
             Industial Pump Manufacturer and Distributor.
+          </Text>
+          <Text color='muted' fontWeight='normal'>
+            Jl. Pangeran Jayakarta 72-74 Blok B3, Jakarta 10730 - Indonesia
+            <br />
+            Telp : +62-021-6011131, 6011132, 6011133, 6011134
+            <br />
+            Fax: +62-021-6011130
           </Text>
         </Stack>
         <Stack
@@ -41,6 +50,9 @@ const Footer = () => (
             <Stack pt='8' justify='space-between' direction={{ base: 'column' }} align='center'>
               <Text fontSize='lg' fontWeight='extrabold'>
                 Contact Us
+              </Text>
+              <Text fontSize='md' fontWeight='normal'>
+                Please contact us for more details :
               </Text>
               <ButtonGroup variant='ghost'>
                 <IconButton
@@ -59,28 +71,18 @@ const Footer = () => (
                 />
                 <IconButton
                   as='a'
-                  href='https://www.instagram.com/laurenncius/'
+                  href='tel:085157311671'
                   target='_blank'
-                  aria-label='Instagram'
-                  icon={<AiOutlineInstagram fontSize='1.25rem' />}
+                  aria-label='Call center'
+                  icon={<BiPhoneCall fontSize='1.25rem' />}
                 />
               </ButtonGroup>
             </Stack>
-            {/* <Stack spacing='4' minW='36' flex='1'>
-              <Text fontSize='sm' fontWeight='semibold' color='subtle'>
-                Legal
-              </Text>
-              <Stack spacing='3' shouldWrapChildren>
-                <Button variant='link'>Privacy</Button>
-                <Button variant='link'>Terms</Button>
-                <Button variant='link'>License</Button>
-              </Stack>
-            </Stack> */}
           </Stack>
         </Stack>
       </Stack>
       <Divider />
-      <Stack align='center' pt='8' pb='12' justify='space-between' direction={{ base: 'column-reverse', md: 'row' }}>
+      <Stack align='center' pb='12' justify='space-between' direction={{ base: 'column-reverse', md: 'row' }}>
         <Text fontSize='sm' color='subtle'>
           &copy; {new Date().getFullYear()} PT. Grahanindo Mecanitron, Inc. All rights reserved.
         </Text>
--- client/src/screens/LandingScreen.jsx
+++ client/src/screens/LandingScreen.jsx
@@ -17,58 +17,90 @@ import { Link as ReactLink } from 'react-router-dom';
 import { AiFillShop } from 'react-icons/ai';
 
 const LandingScreen = () => (
-  <Box maxW='8xl' mx='auto' px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }} minH='3xl'>
-    <Stack direction={{ base: 'column-reverse', lg: 'row' }} spacing={{ base: '0', lg: '20' }}>
+  <>
+    <Box maxW='8xl' mx='auto' px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }} minH='2xl'>
+      <Stack direction={{ base: 'column-reverse', lg: 'row' }} spacing={{ base: '0', lg: '20' }}>
+        <Box
+          width={{ lg: 'sm' }}
+          transform={{ base: 'translateY(-50%)', lg: 'none' }}
+          bg={{ base: useColorModeValue('blue.50', 'gray.700'), lg: 'transparent' }}
+          mx={{ base: '6', md: '8', lg: '0' }}
+          px={{ base: '6', md: '8', lg: '0' }}
+          py={{ base: '6', md: '8', lg: '12' }}
+        >
+          <Stack spacing={{ base: '8', lg: '10' }}>
+            <Stack spacing={{ base: '2', lg: '4' }}>
+              <Flex alignItems='center'>
+                <Icon as={AiFillShop} h={12} w={12} color={useColorModeValue('blue.500', 'blue.300')} />
+                <Text fontSize='4xl' fontWeight='bold' pl='2'>
+                  Grahanindo Store
+                </Text>
+              </Flex>
+              <Heading size='lg' fontWeight='normal'>
+                Selling Pumps and Parts !
+              </Heading>
+            </Stack>
+            <HStack spacing='3'>
+              <Link
+                as={ReactLink}
+                to='/products'
+                color={useColorModeValue('blue.500', 'blue.300')}
+                fontWeight='bold'
+                fontSize='lg'
+                variant='ghost'
+              >
+                <Button colorScheme='blue' variant='ghost'>
+                  Discover now
+                </Button>
+              </Link>
+              <Icon color={useColorModeValue('blue.500', 'blue.300')} as={FaArrowRight} />
+            </HStack>
+          </Stack>
+        </Box>
+        <Flex flex='1' overflow='hidden'>
+          <Image
+            src='images/landing.jpg'
+            alt='Lovely Image'
+            fallback={<Skeleton />}
+            maxH='550px'
+            minW='300px'
+            objectFit='cover'
+            flex='1'
+          />
+        </Flex>
+      </Stack>
+    </Box>
+    {/* About Us Section */}
+    <Box maxW='8xl' mx='auto' px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }}>
       <Box
-        width={{ lg: 'sm' }}
-        transform={{ base: 'translateY(-50%)', lg: 'none' }}
-        bg={{ base: useColorModeValue('blue.50', 'gray.700'), lg: 'transparent' }}
         mx={{ base: '6', md: '8', lg: '0' }}
         px={{ base: '6', md: '8', lg: '0' }}
         py={{ base: '6', md: '8', lg: '12' }}
       >
-        <Stack spacing={{ base: '8', lg: '10' }}>
-          <Stack spacing={{ base: '2', lg: '4' }}>
-            <Flex alignItems='center'>
-              <Icon as={AiFillShop} h={12} w={12} color={useColorModeValue('blue.500', 'blue.300')} />
-              <Text fontSize='4xl' fontWeight='bold' pl='2'>
-                Grahanindo Store
+        <Stack direction={{ base: 'column-reverse', lg: 'row' }}>
+          <Stack>
+            <Box width={{ sm: '400px', md: '600px', lg: '600px' }}>
+              <Heading size='lg'>About Us</Heading>
+              <Heading size='md' pt='2'>
+                PT. Grahanindo Mecanitron
+              </Heading>
+              <Text align='justify' fontSize='md' pt='2'>
+                PT. Grahanindo Mecanitron is an established agency company for a range of reputable engineered products
+                of pumps, pumping systems and fluid handling components. Industries we are mainly servicing are Food &
+                Beverage, Chemical processing, Oil & Gas/Petrochemicals, Paint, Ceramics, Pulp & Paper, Automotive,
+                Industrial, Building/construction and the water and waste water treatment industries.
               </Text>
-            </Flex>
-            <Heading size='lg' fontWeight='normal'>
-              Selling Pumps and Parts !
-            </Heading>
+            </Box>
           </Stack>
-          <HStack spacing='3'>
-            <Link
-              as={ReactLink}
-              to='/products'
-              color={useColorModeValue('blue.500', 'blue.300')}
-              fontWeight='bold'
-              fontSize='lg'
-              variant='ghost'
-            >
-              <Button colorScheme='blue' variant='ghost'>
-                Discover now
-              </Button>
-            </Link>
-            <Icon color={useColorModeValue('blue.500', 'blue.300')} as={FaArrowRight} />
-          </HStack>
+          <Stack>
+            <Box>
+              <Image src='images/logo.png' pt='12' pl={{ base: '0', lg: '16' }} w={600} h={150} />
+            </Box>
+          </Stack>
         </Stack>
       </Box>
-      <Flex flex='1' overflow='hidden'>
-        <Image
-          src='images/landing.jpg'
-          alt='Lovely Image'
-          fallback={<Skeleton />}
-          maxH='550px'
-          minW='300px'
-          objectFit='cover'
-          flex='1'
-        />
-      </Flex>
-    </Stack>
-  </Box>
+    </Box>
+  </>
 );
 
 export default LandingScreen;
